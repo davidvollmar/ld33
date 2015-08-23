21de8a54d46c078e2dfb128c4aec87c64d5829d8
Stef van Schuylenburg <s.b.v.schuylenburg@student.tue.nl>
2015-08-23 22:59:26 +0200

    Merge conflict when pushin to master

--- src/pacman.js
+++ src/pacman.js
@@ -7,10 +7,10 @@ class Pacman extends Entity {
 	constructor () {
 		super("resources/images/inverted/pacman_right_3.png");
 
-		this.addAnimation(Direction.LEFT, ["resources/images/inverted/pacman_left_1.png", "resources/images/inverted/pacman_left_2.png", "resources/images/inverted/pacman_left_3.png", "resources/images/inverted/pacman_left_4.png", "resources/images/inverted/pacman_left_5.png"], true);
-		this.addAnimation(Direction.RIGHT, ["resources/images/inverted/pacman_right_1.png", "resources/images/inverted/pacman_right_2.png", "resources/images/inverted/pacman_right_3.png", "resources/images/inverted/pacman_right_4.png", "resources/images/inverted/pacman_right_5.png"], true);
-		this.addAnimation(Direction.UP, ["resources/images/inverted/pacman_up_1.png", "resources/images/inverted/pacman_up_2.png", "resources/images/inverted/pacman_up_3.png", "resources/images/inverted/pacman_up_4.png", "resources/images/inverted/pacman_up_5.png"], true);
-		this.addAnimation(Direction.DOWN, ["resources/images/inverted/pacman_down_1.png", "resources/images/inverted/pacman_down_2.png", "resources/images/inverted/pacman_down_3.png", "resources/images/inverted/pacman_down_4.png", "resources/images/inverted/pacman_down_5.png"], true);
+		this.addAnimation(Direction.LEFT, ["pacman_left_1.png", "pacman_left_2.png", "pacman_left_3.png", "pacman_left_4.png", "pacman_left_5.png"], true);
+		this.addAnimation(Direction.RIGHT, ["pacman_right_1.png", "pacman_right_2.png", "pacman_right_3.png", "pacman_right_4.png", "pacman_right_5.png"], true);
+		this.addAnimation(Direction.UP, ["pacman_up_1.png", "pacman_up_2.png", "pacman_up_3.png", "pacman_up_4.png", "pacman_up_5.png"], true);
+		this.addAnimation(Direction.DOWN, ["pacman_down_1.png", "pacman_down_2.png", "pacman_down_3.png", "pacman_down_4.png", "pacman_down_5.png"], true);
 	}
 
 	update (dt) {
