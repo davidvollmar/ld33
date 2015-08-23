b3799e92f25f8f56043b9271472ee78c86617002
David Vollmar <david@vollmar.nl>
2015-08-23 14:43:45 +0200

    add missing files

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+//var express = require('express'),
+//    http = require('http');
+//
+//var app = express();
+//var server = http.createServer(app);
+//var io = require('socket.io')(server);
+
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000));
-
+//app.set('port', (process.env.PORT || 5000));
+server.listen(5000);
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -15,6 +24,17 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    socket.emit('chat message','server response');
+  })
 });
 
 
